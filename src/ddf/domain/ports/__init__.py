6be21c50e0a8d4c4771ce6c93bae051d1c1e5ac2
ThiagoLimaC <thiagolima120603@outlook.com>
2026-07-24 15:31:52 -0300

    feat(ports): reexporta Extrator/Gerador em domain/ports/__init__.py

--- src/ddf/domain/ports/__init__.py
+++ src/ddf/domain/ports/__init__.py
@@ -1,1 +1,6 @@
 """Protocols que definem os pontos de extensão do pipeline."""
+
+from ddf.domain.ports.extrator import Extrator
+from ddf.domain.ports.gerador import Gerador
+
+__all__ = ["Extrator", "Gerador"]
